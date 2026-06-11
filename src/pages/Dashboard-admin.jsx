34d7044565a2773195d9cae3568d6fd00b833519
nadiaanndtya <nadiaaninditiya@gmail.com>
2026-06-11 17:39:30 +0800

    Fix CSS import casing

--- src/pages/Dashboard-admin.jsx
+++ src/pages/Dashboard-admin.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import NavbarAdmin from "../components/NavbarAdmin.jsx";
-import "../styles/Admin.css";
+import "../styles/admin.css";
 import foto from "../assets/admin.png";
 
 import {
